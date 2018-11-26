1ec42c203d08c055eb8b998bfb20faa1424ac838
Nora Vogt <noraevogt@gmail.com>
2018-11-26 03:43:53 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,38 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  hash = {
+                  "hello" => "hi",
+                  "to" => "2",
+                  "too" => "2",
+                  "two" => "2",
+                  "be" => "b",
+                  "you" => "u",
+                  "at" => "@",
+                  "and" => "&"
+  }
+end
+
+
+def word_substituter(tweet)
+
+  tweet.split.collect do |word|
+  if dictionary.keys.include?(word.downcase)
+    word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+  end
+
+
+# let(:tweets) {[tweet_one, tweet_two, tweet_three, tweet_four, tweet_five]}
+
+
+def bulk_tweet_shortener(all_tweets)
+  shortened_tweets = []
+  all_tweets.each do |tweets|
+    shortened_tweets << word_substituter(tweets)
+  end
+  puts shortened_tweets.join(' ')
+end
