52b3a91adc0ea6086aed721eae7efcf011f347e5
Nora Vogt <noraevogt@gmail.com>
2018-11-26 03:50:17 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -46,7 +46,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if tweet.length > 140
-    "#{word_substituter(tweet)[0..137]}..."
+    "#{word_substituter(tweet)[0..136]}..."
   else
     tweet
   end
